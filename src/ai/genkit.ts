050cfa357bc208fe8f13864910d3a9cd4b072e99
Karan Singh Sidar <karannetam4@gmail.com>
2025-09-08 16:54:30 +0000

    nahi hua vaise hi hai

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,9 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
-    googleAI(),
+    googleAI({
+      apiKey: "AIzaSyDsgYr-JxE2a9WLCSZRHuZWHfoFai7ek_I"
+    }),
   ],
   model: 'googleai/gemini-2.5-flash',
   enableTracing: true,
