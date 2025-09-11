c8270afd6bd42d547b7c68824ce47d688d66332b
Karan Singh Sidar <karannetam4@gmail.com>
2025-09-11 16:28:26 +0000

    vps me ab static websiteb ki jarurat nahi hai usme to dynemic hota hai t

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,7 +5,7 @@ import {googleAI} from '@genkit-ai/googleai';
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: 'AIzaSyDsgYr-JxE2a9WLCSZRHuZWHfoFai7ek_I',
+      apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
   model: 'googleai/gemini-2.5-flash',
